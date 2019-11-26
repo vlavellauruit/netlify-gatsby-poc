f1b8ab639c5ae1de4911bdf1895102bab0ab75cf
Victoria <victoria.lavella@uruit.com>
2019-11-26 13:05:45 -0300

    playing and cleaning up

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -3,6 +3,7 @@ import React from 'react'
 import Layout from '../../components/Layout'
 import BlogRoll from '../../components/BlogRoll'
 
+
 export default class BlogIndexPage extends React.Component {
   render() {
     return (
@@ -28,7 +29,7 @@ export default class BlogIndexPage extends React.Component {
         <section className="section">
           <div className="container">
             <div className="content">
-              <BlogRoll />
+              <BlogRoll templateKeys={['blog-post', 'template-test']} />
             </div>
           </div>
         </section>
